4aef514ae6f419cf8b1886b22fea8b87efa7c4e1
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-01-09 13:48:32 +0100

    added lodash lab and tas1, task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+console.log(SimpleLightbox);
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const items = galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+}).join(" ");
+
+const galleryList = document.querySelector(".gallery");
+const options = {
+    captionsData: "alt",
+    captionsDelay: 250
+}
+
+galleryList.insertAdjacentHTML('beforeend', items);
+
+
+ new SimpleLightbox('.gallery a', options);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from "@vimeo/player";
 
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+
+iframePlayer.on('timeupdate', throttle( currentTime => {
+    console.log('played the video!');
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds));
+    const timeupdate = localStorage.getItem("videoplayer-current-time");
+    },1000));
+
+ iframePlayer.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
