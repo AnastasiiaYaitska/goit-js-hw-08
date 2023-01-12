e413aa073a6fc181538a561d0a711b45722fb197
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-01-12 14:37:17 +0100

    added task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,10 +5,10 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const iframePlayer = new Player(iframe);
 
-iframePlayer.on('timeupdate', throttle( currentTime => {
-    console.log('played the video!');
+iframePlayer.on('timeupdate', throttle(currentTime => {
     localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime.seconds));
-    const timeupdate = localStorage.getItem("videoplayer-current-time");
+
+    localStorage.getItem("videoplayer-current-time");
     },1000));
 
  iframePlayer.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
